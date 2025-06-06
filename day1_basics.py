fe4c8e42aea0d5b2b99e91c31bf10245234ccc32
Youngil Kim <skasekase@gmail.com>
2025-06-06 12:35:48 +0900

    Initial commit: Day 1 Python warm-up

---
+++ day1_basics.py
@@ -0,0 +1,19 @@
+# Day 1: Python Basics
+
+# Basic variables
+name = "Youngil"
+age = 46
+is_student = True
+
+# Time until program starts
+months_left = 3
+
+# Print an introduction
+print("Hello! My name is", name)
+print("I'm", age, "years old.")
+print("Am I a student?", is_student)
+print("My program starts in", months_left, "months.")
+
+# Type conversation example
+age_str = str(age)
+print("My age as a string is:", age_str)
